a537f906973c89e6b7e7b7ab3a25fdf22ae7b2a5
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-10 13:00:28 -0300

    Make changes for deploy

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const config = require('./utils/config')
 
 const server = http.createServer(app)
 
-server.listen(config.LOCAL, () => {
-  logger.info(`Server running on port ${config.LOCAL}`)
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
 
 module.exports = server
--- utils/config.js
+++ utils/config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
+const PORT = process.env.PORT
 const MONGODB_URI = process.env.MONGO_DB_URI
 const LOCAL = 3001
 
-module.exports = { MONGODB_URI, LOCAL }
+module.exports = { MONGODB_URI, LOCAL, PORT }
