d5e9b2dc08361d27aef63869fc9e0474c1cbe8be
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-10 12:52:36 -0300

    Add initial controllers

---
+++ controllers/notes.js
@@ -0,0 +1,25 @@
+const noteRouser = require('express').Router()
+const Note = require('../models/Note')
+
+noteRouser.get('/', async (request, response) => {
+  const notes = await Note.find({})
+  response.json(notes)
+})
+
+noteRouser.post('/', async (request, response, next) => {
+  const body = request.body
+  const { content, importance} = body
+
+  const newNote = new Note({
+    content,
+    date: new Date(),
+    importance: importance || false
+  })
+
+  try {
+    const savedNote =  await newNote.save()
+    response.json(savedNote)
+  } catch (exception) { next(exception) }
+})
+
+module.exports = noteRouser
