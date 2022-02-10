f838278458a5e1f802557474a90097f538fd7f9a
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-10 12:51:54 -0300

    Add note model from mongoose

---
+++ models/Note.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require('mongoose')
+
+const noteSchema = new Schema({
+  content: {
+    type: String,
+    required: true,
+    minlength: 10
+  },
+  date: {
+    type: Date,
+    required: true
+  },
+  importance: Boolean
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+const Note = model('Note', noteSchema)
+
+module.exports = Note
