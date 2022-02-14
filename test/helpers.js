8ba1f6d52b4737aea79a96f9c2ad41e0d7d3622b
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-14 09:46:46 -0300

    Add a new method to the helper file

--- test/helpers.js
+++ test/helpers.js
@@ -28,9 +28,23 @@ const getAllFromNotes = async () => {
   return { response, contents }
 }
 
+const nonexistentId = async () => {
+  const note = new Note({
+    content: 'This notes will be deleted',
+    date: new Date(),
+    importance: false
+  })
+
+  await note.save()
+  await note.remove()
+
+  return note.id.toString()
+}
+
 module.exports = {
   app,
   api,
   initialNotes,
+  nonexistentId,
   getAllFromNotes
 }
