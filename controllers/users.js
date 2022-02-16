15cc17d3a41b77539d8a177ae00196ece094e17c
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-16 09:01:38 -0300

    Edit post user method to catch an error 400

--- controllers/users.js
+++ controllers/users.js
@@ -8,18 +8,22 @@ userRouter.get('/', async (request, response) => {
 })
 
 userRouter.post('/', async (request, response) => {
-  const { username, name, password } = request.body
+  try {
+    const { username, name, password } = request.body
 
-  const saltRounds = 10
-  const passwordHash = await bcrypt.hash(password, saltRounds)
-  const newUser = new User({
-    username,
-    name,
-    passwordHash
-  })
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+    const newUser = new User({
+      username,
+      name,
+      passwordHash
+    })
 
-  const savedUser = await newUser.save()
-  response.status(201).json(savedUser)
+    const savedUser = await newUser.save()
+    response.status(201).json(savedUser)
+  } catch (error) {
+    response.status(400).json(error)
+  }
 })
 
 module.exports = userRouter
