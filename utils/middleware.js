8cc2e7f7a219f18936a9fed8833877a3347e4ba2
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-02-17 19:47:18 -0300

    Update handleErrors to catch the new errors

--- utils/middleware.js
+++ utils/middleware.js
@@ -14,15 +14,19 @@ const notFound = (req, res, next) => {
 }
 
 const handleErrors = (error, req, res, next) => {
-  console.log(error.name)
-  console.log(error.message)
   if (error.name === 'CastError') {
     res.status(400).send({ error: 'id used is malformed' })
   } else if (error.name === 'ValidationError') {
     res.status(400).send({ error: error.message })
   } else if (error.name === 'TypeError') {
     res.status(404).send({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    res.status(401).send({ error: error.message })
+  } else if (error.name === 'TokenExpiredError') {
+    res.status(401).send({ error: error.message })
   } else {
+    console.log(error.name)
+    console.log(error.message)
     res.status(500).end()
   }
 
